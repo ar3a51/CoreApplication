ce25e6ae432d6b8cddf9f2dadfc19ff37f2c9d9e
ar3a51 <ar3a51@yahoo.com>
2015-11-21 00:23:03 +1100

    initial commit

---
+++ CoreApplication/Infrastructure/EngineContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+using Autofac;
+
+namespace CoreApplication.Infrastructure
+{
+   public class EngineContext
+    {
+        private static ILifetimeScope _container;
+
+        public static void initialize(ILifetimeScope container)
+        {
+            _container = container;
+        }
+
+        private static ILifetimeScope Scope {
+
+            get {
+                if (HttpContext.Current.Items["scope"] == null)
+                    HttpContext.Current.Items["scope"] = _container.BeginLifetimeScope();
+
+                return (ILifetimeScope)HttpContext.Current.Items["scope"];
+            }
+        }
+
+
+        public static T Locate<T>() {
+            return Scope.Resolve<T>();
+        }
+
+        public static T Locate<T>(string name)
+        {
+            return Scope.ResolveKeyed<T>(name);
+        }
+    }
+}
